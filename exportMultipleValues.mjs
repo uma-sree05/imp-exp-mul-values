4849a2c3d14be554a34bb4af32963710856baed3
uma sree <umasreeseelam@gmail.com>
2022-10-25 16:58:18 +0000

    import&export of multiple values

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,8 @@
+let myArray = ["camel", 265, true, "5.6"];
+let bulb = { watts: 10, type: "LED" };
+let multiplyByFour = function (num) {
+  return num * 4;
+};
+export { myArray, bulb, multiplyByFour };
+// export default { bulb };
+// export default { multiplyByFour };
---
+++ importMultipleValues.mjs
@@ -0,0 +1,7 @@
+import { myArray, bulb, multiplyByFour } from "./exportMultipleValues.mjs";
+// import myArray from "./exportMultipleValues";
+// import bulb from "./exportMultipleValues";
+// import multiplyByFour from "./exportMultipleValues";
+console.log(myArray);
+console.log(bulb);
+console.log(multiplyByFour(2));
